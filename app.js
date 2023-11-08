0ad167264ba3d6a6422412dcfb3246ac725e4e97
andyat91 <andres.alves.91@gmail.com>
2023-11-08 18:35:59 +0100

    update polo_digital create news html

--- app.js
+++ app.js
@@ -44,20 +44,23 @@ app.get("/carrusel", function(request,response)  {
 app.get("/login", function(request,response) {
     const email = request.query.email;
     const password = request.query.password;
-    
+ //consulta con usuarios
+ //select * usuarios donde el email es igual al que ponemos en request y contraseña igual   
+ //connection.query tiene una funcion dentro, si error mostrar mensaje y si ok realizar consulta.
     connection.query(`select * from usuarios where email = "${email}" and password = "${password}"`, function(error,result,fields) {
         if (error) {
             response.status(400).send(`error ${error.message}`);
         return;
     } 
-
+//recorreme el array y si no hay ninguna coincidencia entre email y contraseña devuelve "email o contraseña mal"
     if (result.length == 0) {
         response.send({message: "email o contraseña mal"});
-    
+//si encuentra una coincidencia de ambos se loguea.   
     } else {
         response.send({message: "logueado"});
     }
     })
+//connection.query va con parentesus hasta el final de la consulta.
 
 
 
